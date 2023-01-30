e81662cbb6614d1eb4a2724c064ece183b9a6216
nemears <nickmears2@gmail.com>
2023-01-29 21:55:34 -0500

    added package and pseudo multiple inheritance for sets

--- test/elementTest.js
+++ test/elementTest.js
@@ -1,6 +1,7 @@
 import Element from '../lib/element.js';
 import assert from 'assert';
 import Namespace from '../lib/namespace.js';
+import Package from '../lib/package.js';
 
 describe('ElementTest', function () {
   describe('#ownedElement()', function () {
@@ -19,8 +20,40 @@ describe('ElementTest', function () {
       assert.equal(nmspc.ownedMembers.data[0].id, otherNmspc.id);
       assert.equal(nmspc.members.data[0].id, otherNmspc.id);
       assert.equal(nmspc.ownedElements.data[0].id, otherNmspc.id);
-      assert.equal(otherNmspc.namespace.val.id, nmspc.id);
-      assert.equal(otherNmspc.owner.val.id, nmspc.id);
+      
+    });
+  });
+  describe("#packagedElements", function() {
+    it('should add and remove from packagedElement set', function() {
+      let pckg = new Package();
+      let otherPckg = new Package();
+      pckg.packagedElements.add(otherPckg);
+      assert.equal(pckg.packagedElements.data[0].id, otherPckg.id);
+      assert.equal(pckg.ownedMembers.data[0].id, otherPckg.id);
+      assert.equal(pckg.members.data[0].id, otherPckg.id);
+      assert.equal(pckg.ownedElements.data[0].id, otherPckg.id);
+      assert.equal(otherPckg.owningPackage.val.id, pckg.id);
+      assert.equal(otherPckg.namespace.val.id, pckg.id);
+      assert.equal(otherPckg.owner.val.id, pckg.id);
+      let secondPckg = new Package();
+      pckg.packagedElements.add(secondPckg);
+      assert.equal(pckg.packagedElements.data[1].id, secondPckg.id);
+      assert.equal(pckg.ownedMembers.data[1].id, secondPckg.id);
+      assert.equal(pckg.members.data[1].id, secondPckg.id);
+      assert.equal(pckg.ownedElements.data[1].id, secondPckg.id);
+      assert.equal(secondPckg.owningPackage.val.id, pckg.id);
+      assert.equal(secondPckg.namespace.val.id, pckg.id);
+      assert.equal(secondPckg.owner.val.id, pckg.id);
+      pckg.packagedElements.remove(otherPckg);
+      assert.equal(pckg.packagedElements.data[0].id, secondPckg.id);
+      assert.equal(pckg.ownedMembers.data[0].id, secondPckg.id);
+      assert.equal(pckg.members.data[0].id, secondPckg.id);
+      assert.equal(pckg.ownedElements.data[0].id, secondPckg.id);
+      assert.equal(secondPckg.owningPackage.val.id, pckg.id);
+      assert.equal(secondPckg.namespace.val.id, pckg.id);
+      assert.equal(secondPckg.owner.val.id, pckg.id);
+      pckg.packagedElements.remove(secondPckg);
+      assert.equal(pckg.packagedElements.data.length, 0);
     });
   });
 });
