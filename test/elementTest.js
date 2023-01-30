9b814bde5a0f85a9fcecbb9c4d360837412f6fb3
nemears <nickmears2@gmail.com>
2023-01-29 21:14:31 -0500

    added more types and functionality to set

--- test/elementTest.js
+++ test/elementTest.js
@@ -1,5 +1,6 @@
 import Element from '../lib/element.js';
 import assert from 'assert';
+import Namespace from '../lib/namespace.js';
 
 describe('ElementTest', function () {
   describe('#ownedElement()', function () {
@@ -8,6 +9,18 @@ describe('ElementTest', function () {
         let otherEl = new Element();
         el.ownedElements.add(otherEl);
         assert.equal(el.ownedElements.data[0].id, otherEl.id);
+    });
+  });
+  describe('#ownedMembers()', function() {
+    it('should set opposite and subsets when adding element', function() {
+      let nmspc = new Namespace();
+      let otherNmspc = new Namespace();
+      nmspc.ownedMembers.add(otherNmspc);
+      assert.equal(nmspc.ownedMembers.data[0].id, otherNmspc.id);
+      assert.equal(nmspc.members.data[0].id, otherNmspc.id);
+      assert.equal(nmspc.ownedElements.data[0].id, otherNmspc.id);
+      assert.equal(otherNmspc.namespace.val.id, nmspc.id);
+      assert.equal(otherNmspc.owner.val.id, nmspc.id);
     });
   });
 });
