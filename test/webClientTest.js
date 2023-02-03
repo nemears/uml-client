26e57c381ae38fe93ec0efc8fbfcc5607758587c
nemears <nickmears2@gmail.com>
2023-02-03 10:26:40 -0500

    expanding client functionality and added class

--- test/webClientTest.js
+++ test/webClientTest.js
@@ -4,7 +4,7 @@ import assert from 'assert';
 describe("WebClientTest", function () {
     describe("postElement", function() {
         it("should connect to server and ask server to create package for it", async function() {
-            let client = new UmlWebClient();
+            let client = new UmlWebClient(); // i think this might only work in browser, TODO setup in browser
             let pckg = await client.post('package');
             assert.equal(pckg.id.length, 28);
         });
