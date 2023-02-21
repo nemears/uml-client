1146b1a0dd6368866ebefa57e362046c20df9705
nemears <nickmears2@gmail.com>
2023-02-21 10:45:34 -0500

    added ability to iterate through only ids

--- test/setTest.js
+++ test/setTest.js
@@ -23,4 +23,25 @@ describe('SetTest', () => {
             }
         });
     })
+    describe('Ids iterate', () => {
+        it('iterate through ids', () => {
+            const manager = new UmlManager();
+            let pckg = manager.create('package');
+            let class1 = manager.create('class');
+            let class2 = manager.create('class');
+            let class3 = manager.create('class');
+            pckg.packagedElements.add(class1);
+            pckg.packagedElements.add(class2);
+            pckg.packagedElements.add(class3);
+            let i = 0;
+            for (let id of pckg.packagedElements.ids()) {
+                switch (i) {
+                    case 0: assert.equal(id, class1.id); break;
+                    case 1: assert.equal(id, class2.id); break;
+                    case 3: assert.equal(id, class3.id); break;
+                }
+                i++;
+            }
+        });
+    });
 });
